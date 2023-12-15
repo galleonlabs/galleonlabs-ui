1b3a6e09fcdb4b3846b551f85f0d15d79518c785
Davy Jones <hello@galleon.community>
2023-12-15 12:31:21 +0000

    tortuga pic

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -60,11 +60,12 @@ const projects = [
   {
     title: "Tortuga Onchain",
     url: "https://tortugaonchain.com",
-    imageSrc: "/tortuga.jpg",
+    imageSrc: "/tortuga.png",
     imageAlt: "Tortuga onchain logo",
     description: "Actionable crypto-native content for speculators.",
-    detail: "A new project under the flag of Galleon being built to include newsletter and other forms of media content that can be actioned.",
+    detail: "A new project under the flag of Galleon being built with the aim of providing zero bloat content to help investors and traders navigate the crypto markets and use on-chain protocols.",
     badges: [
+      { title: 'Substack', url: 'https://tortugaonchain.substack.com/' },
       { title: 'Website', url: 'https://tortugaonchain.com' },
       { title: 'Twitter', url: 'https://twitter.com/TortugaOnchain' },
     ]
